ae19b1e0657985ed9d9cbe188aa62762db03b479
a2214n <a2214n@gmail.com>
2022-03-12 12:45:29 -0500

    completely lost

--- Develop/script.js
+++ Develop/script.js
@@ -7,17 +7,46 @@ const getKey = {
 }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-  console.log("generate");
-
-// Write password to the #password input
+//var generateBtn = document.querySelector("#generate");
+ // Write password to the #password input
 //function writePassword() {
   //var password = generatePassword();
   //var passwordText = document.querySelector("#password");
 
   //passwordText.value = password;
-
+function createPassword () {
+  const upper = document.getElementById("upperCase").checked;
+  const lower = document.getElementById("lowerCase").checked;
+  const number = document.getElementById("number").checked;
+  const symbol = document.getElementById("symbol").checked;
+  if (upper + lower + number + symbol === 0) {
+    alert("Please check at least one box");
+    return;
+  }
+  const length = document.getElementById("length");
+  let password = "";
+  while (length.value > password.length) {
+    let keyToAdd = getKey [Math.floor(Math.random() * getKey.length)];
+    let isChecked = document.getElementById(keyToAdd.name).checked;
+    if(isChecked) {
+      password += keyToAdd ();
+    }
+  }
+  cardBody.innerHTML = password;
 }
 
+function copyPassword () {
+  const password = document.getElementById("generator").innerText;
+  if (!password) {return;}
+  textarea.value=password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  alert("Password copied to clipboard");
+}
+
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
